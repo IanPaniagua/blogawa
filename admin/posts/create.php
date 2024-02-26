2e6e6663211e0c1a52fa44a58fce2edf67fd8748
Paniagua Ian <fachinformatiker.in.de@gmail.com>
2024-02-26 12:18:57 +0100

    update images($_FILES[][], enctype,time(), move-uploaded-file() )

--- admin/posts/create.php
+++ admin/posts/create.php
@@ -47,7 +47,7 @@
                 <h2 class="page-title">Add Posts</h2>
                 <?php include(ROOT_PATH . "/app/helpers/formErrors.php");  ?>
 
-                <form action="create.php" method="post">
+                <form action="create.php" method="post" enctype="multipart/form-data">
                     <div>
                         <label for="">Title</label>
                         <input type="text" name="title" value="<?php echo $title ?>" class="text-input">
@@ -77,10 +77,18 @@
                         </select>
                     </div>
                     <div>
-                        <label> 
-                                <input type="checkbox" name="published">
-                                Publish
-                        </label>
+                        <?php if (empty($published)): ?>
+                            <label> 
+                                    <input type="checkbox" name="published">
+                                    Publish
+                            </label>
+                        <?php else: ?>
+                            <label> 
+                                    <input type="checkbox" name="published" checked>
+                                    Publish
+                            </label>
+                        <?php endif; ?>
+                    
                     </div>
                     <div>
                         <button type="submit" name="add-post" class="btn btn-big">Add Post</button>
--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -9,6 +9,7 @@ $title = '';
 $body = '';
 $topic_id = '';
 $image = '';
+$published = '';
 
 
 $errors = array();
@@ -16,10 +17,26 @@ $topics = selectAll('topics');
 $posts = selectAll($table);
 
  if (isset($_POST['add-post'])) {
+   // dd($_FILES['image']);
    $errors = validatePost($_POST);
 
+   if (!empty($_FILES['image']['name'])) {
+      $image_name = time(). '_'. $_FILES['image']['name'];
+      $destination = ROOT_PATH . "/assets/images/" . $image_name;
+
+      $result = move_uploaded_file($_FILES['image']['tmp_name'], $destination);
+
+      if($result){
+         $_POST['image'] = $image_name;
+      }else {
+         array_push($errors, "Failed to upload image");
+      }
+   }else {
+      array_push($errors, "Post image required");
+   }
+
    if(count($errors)==0) {
-      unset($_POST['add-post'], $_POST['topic_id']);
+      unset($_POST['add-post']);
       $_POST['user_id'] = 1;
       $_POST['published'] = isset($_POST['published']) ? 1 : 0;
       $_POST['body'] = htmlentities($_POST['body']);
@@ -29,17 +46,11 @@ $posts = selectAll($table);
       $_SESSION['message'] = 'Post created successfully';
       $_SESSION['type'] = 'success';
       
-      exit();
-      
    }else {
       $title = $_POST['title'];
       $body = $_POST['body'];
       $topic_id = $_POST['topic_id'];
-
+      $published =isset($_POST['published']) ? 1 : 0;
    }
-  
-   
-
-
-    }
+}
   
