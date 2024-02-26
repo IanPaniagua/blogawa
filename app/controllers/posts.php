a030ce7ade214a54609cd9600a7f6082b09a68e8
Paniagua Ian <fachinformatiker.in.de@gmail.com>
2024-02-26 15:01:17 +0100

    Logic for update values fom the  post

--- app/controllers/posts.php
+++ app/controllers/posts.php
@@ -5,16 +5,26 @@
 
 $table = 'posts';
 
+$topics = selectAll('topics');
+$posts = selectAll($table);
+
+$errors = array();
+$id = '';
 $title = '';
 $body = '';
 $topic_id = '';
 $image = '';
 $published = '';
-
-
-$errors = array();
-$topics = selectAll('topics');
-$posts = selectAll($table);
+if (isset($_GET['id'])) {
+   $id = $_GET['id'];
+   $post = selectOne($table, ['id' => $id]);
+   $id = $post['id'];
+   $title = $post['title'];
+   $body = $post['body'];
+   $topic_id = $post['topic_id'];
+   $published = $post['published'];
+   // dd($post);
+}
 
  if (isset($_POST['add-post'])) {
    // dd($_FILES['image']);
@@ -54,3 +64,38 @@ $posts = selectAll($table);
    }
 }
   
+if (isset($_POST['update-post'])) {
+   $errors = validatePost($_POST);
+   if (!empty($_FILES['image']['name'])) {
+      $image_name = time(). '_'. $_FILES['image']['name'];
+      $destination = ROOT_PATH . "/assets/images/" . $image_name;
+
+      $result = move_uploaded_file($_FILES['image']['tmp_name'], $destination);
+
+      if($result){
+         $_POST['image'] = $image_name;
+      }else {
+         array_push($errors, "Failed to upload image");
+      }
+   }else {
+      array_push($errors, "Post image required");
+   }
+   if(count($errors)==0) {
+      $id = $_POST['id'];
+      unset($_POST['update-post'],  $_POST['id']);
+      $_POST['user_id'] = 1;
+      $_POST['published'] = isset($_POST['published']) ? 1 : 0;
+      $_POST['body'] = htmlentities($_POST['body']);
+
+      $post_id = update($table, $id, $_POST);
+      header('location: '.BASE_URL. '/admin/posts/index.php');
+      $_SESSION['message'] = 'Post updated successfully';
+      $_SESSION['type'] = 'success';
+      
+   }else {
+      $title = $_POST['title'];
+      $body = $_POST['body'];
+      $topic_id = $_POST['topic_id'];
+      $published =isset($_POST['published']) ? 1 : 0;
+   }
+}
